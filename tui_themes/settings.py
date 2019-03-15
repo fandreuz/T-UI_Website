0bd4639dc0e1f99123bb50d25bed0a89dc2fbecf
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2019-03-16 00:22:30 +0100

    remove importlib. '-' -> '_'

--- tui_themes/settings.py
+++ tui_themes/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import importlib
-
-djdburl = importlib.import_module('dj-database-url')
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,7 +86,7 @@ DATABASES = {
     }
 }
 
-db_from_env = djdburl.config()
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 LANGUAGE_CODE = 'en-us'
