e2ff71546b60d3fb3890d4af6444ac31d2eeb5cf
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2019-03-15 23:47:43 +0100

    first stable release

--- tui_themes/settings.py
+++ tui_themes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cge!2ymvzt@o%f7+5^s12uf81e@i*qzihd0x=4gfv*4*c*ob1i'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
